c92def3525adc9f14ffe8bc3633f473e9b1da60a
Ricky Nguyen <nguyricky@gmail.com>
2023-09-30 20:10:30 -0700

    Added responsive states

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -3,34 +3,36 @@ import { faHeart, faCode } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return (
-        <div className="grid grid-cols-5">
-            <section class="col-span-1"></section>
-            <div className="col-span-3 min-h-[8rem] w-full py-10">
-                <div className="flex flex-row justify-between">
-                    <div className="flex flex-row">
-                        <div className="mr-3 text-sm">
-                            <a href="https://ko-fi.com/rickynguyen">
-                                <p className="text-secondary hover:text-accent">
-                                    <FontAwesomeIcon icon={faHeart} className="icon" /> Support
-                                </p>
-                            </a>
-                        </div>
-                        <div className="mr-3 text-sm">
-                            <a href="https://github.com/nguyricky/keebtype">
-                                <p className="text-secondary hover:text-accent">
-                                    <FontAwesomeIcon icon={faCode} className="icon" /> GitHub
-                                </p>
-                            </a>
-                        </div>
-                    </div>
-                    <div>
-                        <p className="text-secondary text-sm">Copyright © 2023 - All right reserved by Ricky Nguyen</p>
-                    </div>
+      <div className="grid grid-cols-1 md:grid-cols-5">
+        <section className="md:col-span-1"></section>
+        <div className="col-span-1 md:col-span-3 min-h-[8rem] w-full py-5 md:py-10">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="flex flex-row">
+                <div className="mb-3 mr-3 md:mb-0 md:mr-3 text-center md:text-left">
+                    <a href="https://ko-fi.com/rickynguyen">
+                        <p className="text-secondary hover:text-accent text-sm md:text-md">
+                        <FontAwesomeIcon icon={faHeart} className="icon" /> Support
+                        </p>
+                    </a>
+                </div>
+                <div className="mb-3 mr-3 md:mb-0 md:mr-3 text-center md:text-left">
+                    <a href="https://github.com/nguyricky/keebtype">
+                        <p className="text-secondary hover:text-accent text-sm md:text-md">
+                        <FontAwesomeIcon icon={faCode} className="icon" /> GitHub
+                        </p>
+                    </a>
                 </div>
             </div>
-            <section class="col-span-1"></section>
+            <div className="text-center md:text-right">
+              <p className="text-secondary text-sm md:text-md">
+                Copyright © 2023 - All right reserved by Ricky Nguyen
+              </p>
+            </div>
+          </div>
         </div>
-    )
+        <section className="md:col-span-1"></section>
+      </div>
+    );
 };
 
 export default Footer;
--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -2,14 +2,15 @@ import MainLogo from "../../assets/LogoText.svg"
 
 const NavBar = () => {
     return (
-        <div className="grid grid-cols-5 ">
-            <section class="col-span-1"></section>
-            <div className="col-span-3 min-h-[8rem] w-full py-10">
-                <img src={MainLogo} className="w-[210px]"></img>
-            </div>
-            <section class="col-span-1"></section>
+      <div className="grid grid-cols-1 md:grid-cols-5">
+        <section className="md:col-span-1"></section>
+        <div className="col-span-1 md:col-span-2 min-h-[8rem] w-full py-5 md:py-10">
+          <img src={MainLogo} alt="Logo" className="w-[210px] mx-auto md:mx-0" />
         </div>
-    )
-};
-
-export default NavBar;
+        <section className="md:col-span-1"></section>
+      </div>
+    );
+  };
+  
+  export default NavBar;
+  
