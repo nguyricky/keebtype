a35598a1b8bf040f5e20d38c1e889eff92ae788a
Ricky Nguyen <105029551+nguyricky@users.noreply.github.com>
2023-11-08 04:40:17 +0000

    Updated footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoneyBill1Wave, faCode } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyBill1Wave, faCode, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
@@ -9,22 +10,37 @@ const Footer = () => {
           <div className="flex flex-col md:flex-row justify-between items-center pt-5 md:pt-8">
             <div className="flex flex-row">
                 <div className="mb-3 mr-3 md:mb-0 md:mr-4 text-center md:text-left">
+                    <p className="text-secondary hover:text-accent text-sm md:text-[12px] tracking-wider transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-200 cursor-pointer">
+                      <FontAwesomeIcon icon={faEnvelope} className="icon" /> Contact
+                    </p>
+                </div>
+                <div className="mb-3 mr-3 md:mb-0 md:mr-4 text-center md:text-left">
                   <a href="https://ko-fi.com/rickynguyen">
-                      <p className="text-secondary hover:text-accent text-sm md:text-[13px] tracking-wide">
+                      <p className="text-secondary hover:text-accent text-sm md:text-[12px] tracking-wider transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-200">
                         <FontAwesomeIcon icon={faMoneyBill1Wave} className="icon" /> Support
                       </p>
                   </a>
                 </div>
                 <div className="mb-3 mr-3 md:mb-0 md:mr-4 text-center md:text-left">
                   <a href="https://github.com/nguyricky/keebtype">
-                      <p className="text-secondary hover:text-accent text-sm md:text-[13px] tracking-wide">
+                      <p className="text-secondary hover:text-accent text-sm md:text-[12px] tracking-wider transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-200">
                         <FontAwesomeIcon icon={faCode} className="icon" /> GitHub
                       </p>
                   </a>
                 </div>
+                <div className="mb-3 mr-3 md:mb-0 md:mr-4 text-center md:text-left">
+                    <p className="text-secondary hover:text-accent text-sm md:text-[12px] tracking-wider transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-200 cursor-pointer">
+                      <FontAwesomeIcon icon={faDiscord} className="icon" /> Discord
+                    </p>
+                </div>
+                <div className="mb-3 mr-3 md:mb-0 md:mr-4 text-center md:text-left">
+                    <p className="text-secondary hover:text-accent text-sm md:text-[12px] tracking-wider transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-200 cursor-pointer">
+                      <FontAwesomeIcon icon={faTwitter} className="icon" /> Twitter
+                    </p>
+                </div>
             </div>
             <div className="text-center md:text-right">
-              <p className="text-secondary text-sm md:text-[13px] tracking-wide">
+              <p className="text-secondary text-sm md:text-[12px] tracking-wider">
                 GNU 2023 © Ricky Nguyen
               </p>
             </div>
